a18afa0a4948a8fe5002d41c16851afb49f63bab
Pranav K Bhaskar <pkb19032002@gmail.com>
2024-05-01 11:39:20 +0530

    Minor fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
 
-import { ChakraProvider } from '@chakra-ui/react'
-import theme from './utils/theme.ts'
-import { BrowserRouter } from 'react-router-dom'
+import { ChakraProvider } from "@chakra-ui/react"
+import theme from "./utils/theme.ts"
+import { BrowserRouter } from "react-router-dom"
 
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 const queryClient = new QueryClient()
 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <BrowserRouter>
-          <QueryClientProvider client={queryClient}>
-              <App />
-              {/* <ReactQueryDevtools initialIsOpen={false}/> */}
-          </QueryClientProvider>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
